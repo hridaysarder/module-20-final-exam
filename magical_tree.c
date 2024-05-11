b3706c64f8d7e5d6c6373618e46e3c94aa45e634
HridaySarder <hridoyaug12@gmail.com>
2024-05-12 00:01:13 +0600

    added magical tree

--- magical_tree.c
+++ magical_tree.c
@@ -1,8 +1,37 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     int n;
-    scanf("%d",&n);
-    int s=n;
+    scanf("%d", &n);
+    int l = (n + 1) / 2 + 5;
+    int s = l - 1;
+    int k = 1;
+    for (int i = 1; i <= l; i++)
+    {
+        for (int j = 1; j <= s; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= k; j++)
+        {
+            printf("*");
+        }
+        s--;
+        k = k + 2;
+        printf("\n");
+    }
+    int s2 = ((l * 2) - 1 - n) / 2;
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= s2; j++)
+        {
+            printf(" ");
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
     return 0;
 }
