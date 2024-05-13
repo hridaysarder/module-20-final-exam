4bd57d2b89a1b53651478f09284729ece0d22af3
HridaySarder <hridoyaug12@gmail.com>
2024-05-13 18:32:09 +0600

    added jadu_matrix

---
+++ jadu_matrix.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+int main()
+{
+    int row, col;
+    scanf("%d %d", &row, &col);
+    int a[row][col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    int flag = 1;
+    if (row != col)
+    {
+        flag = 0;
+    }
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if ((i == j || i + j == row - 1) && a[i][j] != 1)
+            {
+                flag = 0;
+                break;
+            }
+            else if (i != j && i + j != row - 1 && a[i][j] != 0)
+            {
+                flag = 0;
+                break;
+            }
+        }
+        if (!flag)
+        {
+            break;
+        }
+    }
+    if (flag)
+    {
+        printf("YES\n");
+    }
+    else
+    {
+        printf("NO\n");
+    }
+    return 0;
+}
